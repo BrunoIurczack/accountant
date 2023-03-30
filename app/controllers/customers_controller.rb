308996e945f8cf52fbf0269a2d6c20cac2d1cd7a
Bruno Felipe <bruno@matheuss-MacBook-Air.local>
2023-03-30 18:41:56 -0300

    C.R.U.D.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,36 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: %i [ show edit update destroy ]
 
-    def index
-        @customers = Customer.all
-    end 
+  def index
+    @customers = Customer.all
+  end 
 
+  def show
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def edit
+  end
+    
+  def create
+    @customer = Customer.new(customer_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def set_customer
+      @customers = Customer.find(params[:id])
+    end
+    def customer_params
+        params.require(:customer).permit(:name, :plan, :value, :contact, :tasks, )
+    end
 end
