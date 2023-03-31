837530d55fc55cdcaa6922a1c7e0e69f762432f4
Bruno Felipe <bruno@matheuss-MacBook-Air.local>
2023-03-31 08:34:23 -0300

    To edit controller en config vscond

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,36 +1,32 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: %i [ show edit update destroy ]
+  before_action :set_customer, only: %i[ show edit update destroy ]
 
   def index
     @customers = Customer.all
   end 
 
-  def show
-  end
+  def show; end
 
   def new
     @customer = Customer.new
   end
 
-  def edit
-  end
+  def edit; end
     
   def create
     @customer = Customer.new(customer_params)
   end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 
   private
-
     def set_customer
       @customers = Customer.find(params[:id])
     end
+
     def customer_params
-        params.require(:customer).permit(:name, :plan, :value, :contact, :tasks, )
+        params.require(:customer).permit(:name, :plan, :value, :contact, :tasks)
     end
 end
