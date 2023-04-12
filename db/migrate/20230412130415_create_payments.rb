30e32b40ea7b8d01d0172785d57f01ba55a76124
Bruno Felipe <bruno@matheuss-MacBook-Air.local>
2023-04-12 16:28:19 -0300

    create migration payament

--- db/migrate/20230412130415_create_payments.rb
+++ db/migrate/20230412130415_create_payments.rb
@@ -1,7 +1,9 @@
 class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
-
+      t.date :month
+      t.booleano :status
+      t.refereces :customers 
       t.timestamps
     end
   end
